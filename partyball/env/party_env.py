ff1c6d5cc46862008c6824ffa760308f4108b852
GeneraleLee <antoine-rollet@live.fr>
2021-04-28 12:11:07 +0200

    Including the observation wrapper in the environment so that stable baselines can use the env

--- partyball/env/party_env.py
+++ partyball/env/party_env.py
@@ -1,14 +1,15 @@
 from gfootball.env.football_env import FootballEnvBase
 from gfootball.env.football_env_core import FootballEnvCore
-from party_env_core import PartyEnvCore
+from partyball.env.party_env_core import PartyEnvCore
 
 
 class PartyEnv(FootballEnvBase):
 
-  def __init__(self, config):
+  def __init__(self, config, wrapper):
     super().__init__(config)
     self._env = PartyEnv._get_environment_core(
             self._config['environment_core'])(self._config)
+    self.obs_wrapper = wrapper(self)
 
   _environment_cores = {
       'gfootball': FootballEnvCore,
@@ -22,3 +23,23 @@ class PartyEnv(FootballEnvBase):
               ', '.join(PartyEnv._environment_cores.keys())))
 
     return PartyEnv._environment_cores[core_type]
+  def reset(self):
+      self._env.reset()
+      return self.obs_wrapper.observation([self._env._observation])
+
+  def step(self, action):
+      action = self._action_to_list(action)
+      if self._agent:
+          # self._agent.set_action(action)
+        pass
+      else:
+          assert len(
+              action
+          ) == 0, 'step() received {} actions, but no agent is playing.'.format(
+              len(action))
+
+      # _, reward, done, info = self._env.step(self._get_actions())
+      raw_obs, raw_reward, done, info = self._env.step(action)
+
+      raw_obs = self.obs_wrapper.observation([raw_obs])
+      return raw_obs, raw_reward, done, info
