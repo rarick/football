f72b14c618bf6778c9b96d43afaf83cef59dffd5
GeneraleLee <antoine-rollet@live.fr>
2021-04-28 12:14:03 +0200

    Updated the reward function because the env does not know the opponent's action

--- partyball/env/party_env_core.py
+++ partyball/env/party_env_core.py
@@ -8,35 +8,25 @@ class PartyEnvCore(FootballEnvCore):
   def __init__(self, config):
     super().__init__(config)
     self.accumulated_passes = 0
+    self.possession_count = [0, 0]
 
   def compute_reward(self, action):
     reward = 0
-    action_cutoff = len(action) // 2
-    left_actions = action[:action_cutoff]
-    right_actions = action[action_cutoff:]
 
     curr_owned = self._observation['ball_owned_team']
-    prev_owned = self._state.prev_ball_owned_team
 
-    if (current_owned != -1 and
-        prev_owned != -1 and
-        current_owned != prev_owned):
-      self.accumulated_passes = 0
+    if curr_owned != -1:
+      self.possession_count[curr_owned] += 1
+      self.possession_count[1 - curr_owned] = 0
 
     if curr_owned == 0:
-      reward += 0.001
-      if (left_actions[self._observation['ball_owned_player']] in
-          PartyEnvCore._pass_actions):
-        self.accumulated_passes += 1
+      reward += 0.001 * self.possession_count[0]
     elif curr_owned == 1:
-      reward -= 0.001
-      if (right_actions[self._observation['ball_owned_player']] in
-          PartyEnvCore._pass_actions):
-        self.accumulated_passes += 1
+      reward -= 0.001 * self.possession_count[1]
 
     score_diff = self._observation['score'][0] - self._observation['score'][1]
     goal_diff = score_diff - self._state.previous_score_diff
 
-    reward += goal_diff + goal_diff*self.accumulated_passes*0.1
+    reward += goal_diff
 
     return reward
