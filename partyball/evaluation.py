caf4807db6708df5f6ada3fadac9bace29bdb39f
GeneraleLee <antoine-rollet@live.fr>
2021-05-01 13:57:02 +0200

    cleaning stuff

--- partyball/evaluation.py
+++ partyball/evaluation.py
@@ -3,6 +3,7 @@
 import click
 import multiprocessing
 import os
+from time import time
 import numpy as np
 
 from baselines import logger
@@ -16,9 +17,23 @@ from partyball.party_wrapper import PartyObservationWrapper
 from gfootball.env import wrappers
 
 
+def create_eval_party_env(config):
+  config.update({
+    'write_goal_dumps': False,
+    'write_full_episode_dumps': False,
+    'render': False,
+    'dump_frequency': 0
+  })
+
+  env = PartyEnv(config)
+  env.disable_render()
+  env = PartyObservationWrapper(env)
+
+  return env
+
+
 def create_single_party_env(iprocess, config):
-  if iprocess != 0:
-    config.update({
+  config.update({
       'write_goal_dumps': False,
       'write_full_episode_dumps': False,
       'render': False,
@@ -26,7 +41,6 @@ def create_single_party_env(iprocess, config):
     })
 
   env = PartyEnv(config)
-  env.disable_render()
   env = PartyObservationWrapper(env)
   env = wrappers.SingleAgentObservationWrapper(env)
   env = wrappers.SingleAgentRewardWrapper(env)
@@ -36,14 +50,15 @@ def create_single_party_env(iprocess, config):
   return env
 
 
-def evaluate(config, load_path):
+def evaluate(config, load_path, n_evals):
   import tensorflow.compat.v1 as tf
-
+  n_envs = 1
   vec_env = SubprocVecEnv([
     (lambda _i=i: create_single_party_env(_i, config))
-    for i in range(1)
+    for i in range(n_envs)
   ], context=None)
-
+  # obs = np.array(vec_env.reset())
+  t_start = time()
   ncpu = multiprocessing.cpu_count()
   tf_config = tf.ConfigProto(allow_soft_placement=True,
                              intra_op_parallelism_threads=ncpu,
@@ -53,50 +68,51 @@ def evaluate(config, load_path):
   tf.Session(config=tf_config).__enter__()
 
   model = ppo2.learn(network='lstm',
-             total_timesteps=0,
+             total_timesteps=-1,
              env=vec_env,
-             seed=None, #Seeding always give the same results, there's some sampling in the lstm
+             seed=None,  #Seeding always give the same results, there's some sampling in the lstm
              nsteps=128,
-             nminibatches=1,
-             noptepochs=4,
-             max_grad_norm=0.5,
-             gamma=0.993,
-             ent_coef=0.01,
-             lr=0.001,
-             log_interval=1,
-             save_interval=100,
-             cliprange=0.27,
+             nminibatches=n_envs,
              load_path=load_path)
-
   model.load(load_path)
+  print("Model loaded in {} seconds.".format(round(time()- t_start, 3)))
+  print("Starting evaluation.")
 
-  env = create_single_party_env(0, config)
-  obs = env.reset()
+  scores_diff = []
+  env = create_eval_party_env(config)
+  for i in range(n_evals):
+      t_start = time()
+      obs = env.reset()
 
-  def initialize_placeholders(nlstm=128, **kwargs):
-      return np.zeros((1, 2 * 128)), np.zeros((1))
+      def initialize_placeholders():
+          return np.zeros((1, 2 * 128)), np.zeros((1))
 
-  state, done = initialize_placeholders()
-  rewards = []
-  while not done:
-      actions, values, state, neglogprobs = model.step(obs, S=state, M=done)
-      obs, rews, done, infos = env.step(actions)
-      done = np.array([done], dtype="float32")
-      rewards.append(rews)
-  print(np.sum(rewards))
+      state, done = initialize_placeholders()
+      while not done:
+          actions, values, state, neglogprobs = model.step(obs, S=state, M=done)
+          obs, rews, done, infos = env.step(actions)
+          done = np.array([done], dtype="float32")
+      score = env.score
+      score_diff = score[0] - score[1]
+      scores_diff.append(score_diff)
+      print("Ran evaluation #{} in {} seconds. Score diff:{}".format(i+1, round(time()-t_start,3), score_diff), end="\n")
+  print("\nMean score difference: {}".format(np.mean(scores_diff)))
+
 
 @click.command()
 @click.argument('config_path', type=click.Path(
   exists=True, dir_okay=False, resolve_path=True))
 @click.argument('load_path', type=click.Path(
   exists=True, dir_okay=False, resolve_path=True))
-def main(config_path, load_path):
+@click.option('--n_evals', default=1)
+def main(config_path, load_path, n_evals):
   '''Run PartyFootball using a YAML config file'''
 
   party_config = PartyConfig.from_yaml(config_path)
 
   # TODO: Check if training mode
-  evaluate(party_config, load_path)
+  evaluate(party_config, load_path, n_evals)
+
 
 if __name__ == '__main__':
   main()
