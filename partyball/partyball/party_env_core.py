b2ba11913bc095c1b48bdddfd6b7c6d191049586
Tyler Rarick <tyler.rarick@gmail.com>
2021-05-02 17:29:18 +0200

    Updated reward function

--- partyball/partyball/party_env_core.py
+++ partyball/partyball/party_env_core.py
@@ -20,18 +20,26 @@ class PartyEnvCore(FootballEnvCore):
     prev_owned_player = self._prev_ball_owned_player
     self._prev_ball_owned_player = curr_owned_player
 
+    # Possession changed
     if (curr_owned_team != -1 and
         prev_owned_team != -1 and
         curr_owned_team != prev_owned_team):
       self.accumulated_passes = 0
 
+    # Someone has possession
     if curr_owned_team != -1:
 
       if curr_owned_team != prev_owned_team:
         self.accumulated_passes = 0
-      elif curr_owned_player == prev_owned_player:
+      elif curr_owned_player != prev_owned_player:
+        # Reward a pass
         self.accumulated_passes += 1
+        if curr_owned_team == 0:
+          reward += 0.01
+        elif curr_owned_team == 1:
+          reward -= 0.01
 
+      # Reward possession
       if curr_owned_team == 0:
         reward += 0.001
       elif curr_owned_team == 1:
@@ -40,6 +48,7 @@ class PartyEnvCore(FootballEnvCore):
     score_diff = self._observation['score'][0] - self._observation['score'][1]
     goal_diff = score_diff - self._state.previous_score_diff
 
+    # Someone scored
     reward += goal_diff + goal_diff*self.accumulated_passes*0.1
 
     return reward
