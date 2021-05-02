f37abf66c18ee4f6576241695dd2903d6ee68060
GeneraleLee <antoine-rollet@live.fr>
2021-05-02 18:19:23 +0200

    Adding distance to goal which weights rewards for possession and shooting

--- partyball/partyball/party_env_core.py
+++ partyball/partyball/party_env_core.py
@@ -9,6 +9,7 @@ class PartyEnvCore(FootballEnvCore):
     super().__init__(config)
     self.accumulated_passes = 0
     self._prev_ball_owned_player = None
+    self.max_dist_to_net = 2.04362423 #sqrt([1 - (-1)]**2 + [0.42 - 0]**2)
 
   def compute_reward(self, action):
     reward = 0
@@ -39,11 +40,26 @@ class PartyEnvCore(FootballEnvCore):
         elif curr_owned_team == 1:
           reward -= 0.01
 
-      # Reward possession
+      # Reward possession and shots according to distance to the opponent's net
       if curr_owned_team == 0:
-        reward += 0.001
+
+        player_coords = self._observation["left_team"][curr_owned_player]
+        dist_to_net = ((player_coords[0] - 1)**2 + (player_coords[1] - 0)**2)**(.5)
+        norm_dist_to_net = 1 - dist_to_net / self.max_dist_to_net
+        if action[0]._name == "shot":
+          reward += 0.1*norm_dist_to_net
+        else:
+          reward += 0.01*norm_dist_to_net
+
       elif curr_owned_team == 1:
-        reward -= 0.001
+
+        player_coords = self._observation["right_team"][curr_owned_player]
+        dist_to_net = ((player_coords[0] + 1) ** 2 + (player_coords[1] - 0) ** 2) ** (.5)
+        norm_dist_to_net = 1 - dist_to_net / self.max_dist_to_net
+        if action[1]._name == "shot":
+          reward -= 0.1*norm_dist_to_net
+        else:
+          reward -= 0.01*norm_dist_to_net
 
     score_diff = self._observation['score'][0] - self._observation['score'][1]
     goal_diff = score_diff - self._state.previous_score_diff
